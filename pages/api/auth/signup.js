d6dfc8e4c13b778960aa436d2d3fbedb99f793da
Sergui Morejon <serguinajibm20@gmail.com>
2023-11-23 22:26:36 -0500

    Lesson 6: Adding signup API route

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,17 @@
 import { hashPassword } from '../../../lib/auth';
 import { connectToDatabase } from '../../../lib/db';
 
+/**
+ * 1.1: Adding signup route.
+ * 
+ * We will first of all have to add the logic to create users because "NextAuth" 
+ * can then help us with authenticating those users and with getting the token 
+ * for those users, but to create the users themselves, we need to bring our own logic.
+ * 
+ * So, this API route receives a request with an email and a password.
+ * This data should then be used to store that user, and we will use MongoDB again 
+ * as a database for storing the user data. MongoDB atlas is also very useful to use.
+ */
 async function handler(req, res) {
   if (req.method !== 'POST') {
     return;
@@ -35,6 +46,19 @@ async function handler(req, res) {
     return;
   }
 
+  /**
+   * 1.3: Adding signup route.
+   * 
+   * Keep in mind that for the passwords we should never store the plain password 
+   * in the database because if our database ever gets compromised by someone 
+   * or a third-party intruder, then all our user credentials would be usable. 
+   * Hence, for security reasons, it is a best practice and strongly recommended 
+   * that you don't store plain passwords in the database,
+   * but that you instead encrypt them such that they can't be decrypted.
+   * We will still be able to verify them later, no worries, but we want to make sure 
+   * that we store passwords in encrypted form. To encrypt passwords, we can 
+   * use a third-party package "bcryptjs".
+   */
   const hashedPassword = await hashPassword(password);
 
   const result = await db.collection('users').insertOne({
